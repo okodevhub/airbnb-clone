7b79d3e577ca9c4b68291577c6f38541016d5c6b
Juhee Han <81957899+okodevhub@users.noreply.github.com>
2022-05-24 21:17:32 +0900

    First Model Fields

--- users/models.py
+++ users/models.py
@@ -4,4 +4,19 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
 
-    bio = models.TextField(default="")
+    """Custom User Model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "other"),
+    )
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
