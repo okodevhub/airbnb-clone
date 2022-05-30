9d138a1cb55fb57f9c98582d05782420c583f44b
Juhee Han <81957899+okodevhub@users.noreply.github.com>
2022-05-30 20:39:33 +0900

    Review Admin and Room Average

--- rooms/models.py
+++ rooms/models.py
@@ -55,7 +55,7 @@ class Photo(core_models.TimeStampedModel):
 
     caption = models.CharField(max_length=80)
     file = models.ImageField()
-    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
@@ -91,3 +91,11 @@ class Room(core_models.TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+    def total_rating(self):
+        all_reviews = self.reviews.all()
+        all_ratings = []
+        for review in all_reviews:
+            all_ratings.append(review.rating_average())
+
+        return 0
