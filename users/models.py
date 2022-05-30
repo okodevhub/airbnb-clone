682b72482634a947cadf48e6d8c2266eca722acf
Juhee Han <81957899+okodevhub@users.noreply.github.com>
2022-05-31 06:15:00 +0900

    Configuring User Uploads part One

--- users/models.py
+++ users/models.py
@@ -36,7 +36,7 @@ class User(AbstractUser):
         (CURRENCY_JPY, "JPY"),
     )
 
-    avatar = models.ImageField(blank=True)
+    avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(blank=True, null=True)
