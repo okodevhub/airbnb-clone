e7c6836d51cc9f5751f89d69ebbbd57838df2e03
Juhee Han <81957899+okodevhub@users.noreply.github.com>
2022-05-30 21:06:37 +0900

    Reservations Admin

--- rooms/models.py
+++ rooms/models.py
@@ -94,8 +94,8 @@ class Room(core_models.TimeStampedModel):
 
     def total_rating(self):
         all_reviews = self.reviews.all()
-        all_ratings = []
+        all_ratings = 0
         for review in all_reviews:
-            all_ratings.append(review.rating_average())
+            all_ratings += review.rating_average()
 
-        return 0
+        return all_ratings / len(all_reviews)
