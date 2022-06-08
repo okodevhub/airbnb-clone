5e410d3afcae4e3383d564c25454538dac64a3b3
Juhee Han <81957899+okodevhub@users.noreply.github.com>
2022-06-09 02:13:42 +0900

    Forms are Awesome!

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,5 @@
-from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic import ListView, DetailView, UpdateView, View
 from django.shortcuts import render
-from django.http import Http404
-from django_countries import countries
 from . import models, forms
 
 # Create your views here.
@@ -23,11 +21,83 @@ class RoomDetail(DetailView):
     model = models.Room
 
 
-def search(request):
+class SearchView(View):
 
-    form = forms.SeachForm()
+    """SearchView Definition"""
 
-    return render(request, "rooms/search.html", {"form": form})
+    def get(self, request):
+
+        country = request.GET.get("country")
+
+        if country:
+
+            form = forms.SeachForm(request.GET)
+
+            if form.is_valid():
+
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
+
+                print(city, price)
+
+                filter_args = {}
+
+                if city != "Anywhere":
+                    filter_args["city__startswith"] = city
+
+                filter_args["country"] = country
+
+                if room_type is not None:
+                    filter_args["room_type"] = room_type
+
+                if price is not None:
+                    filter_args["price__lte"] = price
+
+                if guests is not None:
+                    filter_args["guests__gte"] = guests
+
+                if bedrooms is not None:
+                    filter_args["bedrooms__gte"] = bedrooms
+
+                if beds is not None:
+                    filter_args["beds__gte"] = beds
+
+                if baths is not None:
+                    filter_args["baths__gte"] = baths
+
+                if instant_book is True:
+                    filter_args["instant_book"] = True
+
+                if superhost is True:
+                    filter_args["host__superhost"] = True
+
+                rooms = models.Room.objects.filter(**filter_args)
+
+                for amenity in amenities:
+                    rooms = rooms.filter(amenities=amenity)
+
+                for facility in facilities:
+                    rooms = rooms.filter(facilities=facility)
+
+                return render(
+                    request,
+                    "rooms/search.html",
+                    {"form": form, "rooms": rooms},
+                )
+
+        else:
+            form = forms.SeachForm()
+            return render(request, "rooms/search.html", {"form": form})
 
 
 class EditRoomView(UpdateView):
